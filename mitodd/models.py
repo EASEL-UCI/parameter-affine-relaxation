22b6bc409d336c8dce56103e0c559609d2a2c994
Derek Fan <derekfan.18.2002@gmail.com>
2024-08-24 18:08:28 -0700

    made explicit classes for model infrastructure

--- mitodd/models.py
+++ mitodd/models.py
@@ -1,43 +1,95 @@
 #!/usr/bin/python3
 
+from abc import ABC, abstractmethod
+
 import casadi as cs
 import numpy as np
 
 
-class QuadrotorModel():
+PARAMETER_CONFIG  = {
+    "m": float,
+    "Ixx": float,
+    "Iyy": float,
+    "Izz": float,
+    "Ax": float,
+    "Ay": float,
+    "Az": float,
+    "kf": float,
+    "km": float,
+    "umin": float,
+    "umax": float,
+    "xB": np.ndarray,
+    "yB": np.ndarray,
+}
+
+
+class ModelParameters():
+    def __init__(self, **kwargs) -> None:
+        for key, value in kwargs.items():
+            assert key in PARAMETER_CONFIG.keys()
+            assert type(value) == PARAMETER_CONFIG[key]
+            setattr(self, key, value)
+
+
+class Model(ABC):
+    @property
+    @abstractmethod
+    def xdot(self) -> cs.SX:
+        pass
+    
+    @property
+    @abstractmethod
+    def x(self) -> cs.SX:
+        pass
+    
+    @property
+    @abstractmethod
+    def u(self) -> cs.SX:
+        pass
+    
+    @property
+    @abstractmethod
+    def parameters(self) -> ModelParameters:
+        pass
+
+
+class QuadrotorModel(Model):
     def __init__(
         self,
-        parameters: dict,
-        name="QuadrotorModel",
+        parameters: ModelParameters,
     ) -> None:
-        self.name = name
-        config = self._parameter_config()
-        for key, value in parameters:
-            assert key in config.keys()
-            assert type(value) == config[key]
-            setattr(self, key, value)
-        self._model = self._get_model()
+        self._set_model(parameters)
+        self._parameters = parameters
 
     @property
     def xdot(self):
-        return self._model["xdot"]
+        return self._xdot
 
     @property
     def x(self):
-        return self._model["x"]
-    
+        return self._x
+
     @property
     def u(self):
-        return self._model["u"]
+        return self._u
 
-    def _parameter_config():
-        return {
-            "m": float, "Ixx": float, "Iyy": float, "Izz": float,
-            "Ax": float, "Ay": float, "Az": float, "xB": np.ndarray, "yB": np.ndarray,
-            "kf": float, "km": float, "umin": float, "umax": float, "name": str,
-        }
+    @property
+    def parameters(self) -> ModelParameters:
+        return self._parameters
 
-    def _get_model(self) -> dict:
+    def _set_model(
+        self,
+        param: ModelParameters
+    ) -> None:
+        model = self._derive_model(param)
+        self._xdot = model["xdot"]
+        self._x = model["x"]
+        self._u = model["u"]
+
+    def _derive_model(
+        self,
+        param: ModelParameters
+    ) -> dict:
         x = cs.SX.sym("x")
         y = cs.SX.sym("y")
         z = cs.SX.sym("z")
@@ -53,8 +105,7 @@ class QuadrotorModel():
         r = cs.SX.sym("r")
 
         X = cs.SX(cs.vertcat(
-            x, y, z, q0, q1, q2, q3,
-            xdot, ydot, zdot, p, q, r
+            x, y, z, q0, q1, q2, q3, xdot, ydot, zdot, p, q, r
         ))
 
         # rotation matrix
@@ -65,16 +116,16 @@ class QuadrotorModel():
         ))
 
         # drag terms
-        A = cs.SX(np.diag([self.Ax, self.Ay, self.Az]))
+        A = cs.SX(np.diag([param.Ax, param.Ay, param.Az]))
 
         # Diagonal of inertial matrix
-        J = cs.SX(np.diag([self.Ixx, self.Iyy, self.Izz]))
+        J = cs.SX(np.diag([param.Ixx, param.Iyy, param.Izz]))
 
         # control allocation matrix
         B = cs.SX(cs.vertcat(
-            self.kf * self.yB.reshape(1, self.yB.shape[0]),
-            self.kf * -self.xB.reshape(1, self.xB.shape[0]),
-            self.km * cs.horzcat(-1, 1, -1, 1),
+            param.kf * param.yB.reshape(1, param.yB.shape[0]),
+            param.kf * -param.xB.reshape(1, param.xB.shape[0]),
+            param.km * cs.horzcat(-1, 1, -1, 1),
         ))
 
         # gravity vector
@@ -83,52 +134,48 @@ class QuadrotorModel():
         # thrust of motors 1 to 4
         U = cs.SX.sym("u", 4)
         T = cs.SX(cs.vertcat(
-            0, 0, self.kf * (self.u[0]+self.u[1]+self.u[2]+self.u[3])
+            0, 0, param.kf *
+                (U[0]+U[1]+U[2]+U[3])
         ))
 
         # double integrator dynamics
         qv = cs.SX(cs.vertcat(q1,q2,q3))
         v = cs.SX(cs.vertcat(xdot, ydot, zdot))
         wB = cs.SX(cs.vertcat(p, q, r))
-
         Xdot = cs.SX(cs.vertcat(
             v,
             -cs.dot(qv, 0.5*wB),
             0.5 * q0 * wB + cs.cross(qv, wB),
-            (R @ T - A @ v) / self.m + g,
-            cs.inv(J) @ (B @ self.u - cs.cross(wB, J @ wB))
+            (R @ T - A @ v) / param.m + g,
+            cs.inv(J) @ (B @ U - cs.cross(wB, J @ wB))
         ))
 
         return {"xdot": Xdot, "x": X, "u": U}
 
 
-class LinearizedQuadrotor(QuadrotorModel):
+class LinearizedQuadrotorModel(QuadrotorModel):
     def __init__(
         self,
-        parameters: dict,
+        parameters: ModelParameters,
         xref: cs.SX,
         uref: cs.SX,
-        t: cs.SX,
-        name="LinearizedQuadrotorModel"
     ) -> None:
-        super().__init__(parameters, name)
-        self._model = self._linearize_model(self.model, xref, uref, t)
+        super().__init__(parameters)
+        self._linearize_model(xref, uref)
 
     def _linearize_model(
         self,
-        model: dict,
         xref: cs.SX,
         uref:cs.SX,
-        t: cs.SX,
     ) -> dict:
-        A = cs.jacobian(model["xdot"], model["x"])
-        A = cs.substitute(A, model["x"], xref)
-        
-        B = cs.jacobian(model["xdot"], model["u"])
-        B = cs.substitute(B, model["u"], uref)
-        
-        model["xdot"] = A @ model["x"] + B @ model["u"]
-        return model
+        A = cs.jacobian(self.xdot, self.x)
+        A = cs.substitute(A, self.x, xref)
+
+        B = cs.jacobian(self.xdot, self.u)
+        B = cs.substitute(B, self.u, uref)
+
+        self.xdot = A @ self.x + B @ self.u
+
 
 def Crazyflie(
     Ax: float,
@@ -140,19 +187,20 @@ def Crazyflie(
     https://www.research-collection.ethz.ch/handle/20.500.11850/214143
     """
 
-    cf_params = {
-        "Ax": Ax,
-        "Ay": Ay,
-        "Az": Az,
-        "m": 0.027,
-        "Ixx": 1.6571710 * 10**-5,
-        "Iyy": 1.6655602 * 10**-5,
-        "Izz": 2.9261652 * 10**-5,
-        "xB" : 0.0283 * np.array([1, 1, -1, -1]),
-        "yB" : 0.0283 * np.array([1, -1, -1, 1]),
-        "kf" : 1.0,
-        "umax" : 0.15 * np.ones(4),
-        "km" : 0.005964552,
-        "umin" : np.zeros(4),
-    }
+    cf_params = ModelParameters(
+        Ax=Ax,
+        Ay=Ay,
+        Az=Az,
+        m=0.027,
+        Ixx=1.6571710 * 10**-5,
+        Iyy=1.6655602 * 10**-5,
+        Izz=2.9261652 * 10**-5,
+        kf=1.0,
+        km=0.005964552,
+        umin=0.0,
+        umax=0.15,
+        xB=0.0283 * np.array([1, 1, -1, -1]),
+        yB=0.0283 * np.array([1, -1, -1, 1]),
+    )
+
     return QuadrotorModel(cf_params)
