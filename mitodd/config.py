b621ba6097ab97d80c6b82890315d8c31c88e7e3
Derek Fan <derekfan.18.2002@gmail.com>
2024-08-25 02:24:17 -0700

    added physical constants and bounds

--- mitodd/config.py
+++ mitodd/config.py
@@ -4,6 +4,10 @@ import numpy as np
 
 from mitodd.helpers import get_dimensions, get_all_dimensions
 
+CONTACT_ACTIVATION_FACTOR = 500.0
+CONTACT_SPRING_FACTOR = 100.0
+CONTACT_DAMPER_FACTOR = 100.0
+GRAVITY = 9.81
 
 PARAMETER_CONFIG = {
     "m":    {"type": float},
@@ -21,30 +25,51 @@ PARAMETER_CONFIG = {
     "yB":   {"type": np.ndarray, "member_type": np.float64, "dimensions": 4},
 }
 
-
-STATE_VECTOR_CONFIG = {
-    "DRONE_POSITION":         {"type": np.ndarray, "member_type": np.float64, "dimensions": 3},
-    "DRONE_ORIENTATION":      {"type": np.ndarray, "member_type": np.float64, "dimensions": 4},
-    "DRONE_LINEAR_VELOCITY":  {"type": np.ndarray, "member_type": np.float64, "dimensions": 3},
-    "DRONE_ANGULAR_VELOCITY": {"type": np.ndarray, "member_type": np.float64, "dimensions": 3},
-    "PAYLOAD_POSITION_0":     {"type": np.ndarray, "member_type": np.float64, "dimensions": 3},
-    "PAYLOAD_VELOCITY_0":     {"type": np.ndarray, "member_type": np.float64, "dimensions": 3},
+STATE_CONFIG = {
+    "DRONE_POSITION": {
+        "type": np.ndarray, "member_type": np.float64, "dimensions": 3,
+        "lower_bound": [None, None, 0.0], "upper_bound": [None, None, None],
+    },
+    "DRONE_ORIENTATION": {
+        "type": np.ndarray, "member_type": np.float64, "dimensions": 4,
+        "lower_bound": [0.0, 0.0, 0.0, 0.0], "upper_bound": [1.0, 1.0, 1.0, 1.0],
+    },
+    "DRONE_LINEAR_VELOCITY": {
+        "type": np.ndarray, "member_type": np.float64, "dimensions": 3,
+        "lower_bound": [None, None, None], "upper_bound": [None, None, None],
+    },
+    "DRONE_ANGULAR_VELOCITY": {
+        "type": np.ndarray, "member_type": np.float64, "dimensions": 3,
+        "lower_bound": [None, None, None], "upper_bound": [None, None, None],
+    },
+    "PAYLOAD_POSITION_0": {
+        "type": np.ndarray, "member_type": np.float64, "dimensions": 3,
+        "lower_bound": [None, None, None], "upper_bound": [None, None, None],
+    },
+    "PAYLOAD_VELOCITY_0": {
+        "type": np.ndarray, "member_type": np.float64, "dimensions": 3,
+        "lower_bound": [None, None, None], "upper_bound": [None, None, None]
+    },
 }
 
-
-INPUT_VECTOR_CONFIG = {
-    "DRONE_THRUSTS":   {"type": np.ndarray, "member_type": np.float64, "dimensions": 4},
-    "PAYLOAD_RELEASE": {"type": np.ndarray, "member_type": np.int64,   "dimensions": 1},
+INPUT_CONFIG = {
+    "DRONE_THRUSTS": {
+        "type": np.ndarray, "member_type": np.float64, "dimensions": 4,
+        "lower_bound": [0, 0, 0, 0], "upper_bound": [None, None, None],
+    },
+    "PAYLOAD_RELEASE": {
+        "type": np.ndarray, "member_type": np.int64, "dimensions": 1,
+        "lower_bound": [0], "upper_bound": [1],
+    },
 }
 
-
-MODEL_TYPES = {
+MODEL_TYPE_CONFIG = {
     "DRONE": {
-        "state": {"dimensions": get_dimensions(config=STATE_VECTOR_CONFIG, id="DRONE")},
-        "input": {"dimensions": get_dimensions(config=INPUT_VECTOR_CONFIG, id="DRONE")},
+        "state": {"dimensions": get_dimensions(config=STATE_CONFIG, id="DRONE")},
+        "input": {"dimensions": get_dimensions(config=INPUT_CONFIG, id="DRONE")},
     },
     "PAYLOAD": {
-        "state": {"dimensions": get_all_dimensions(config=STATE_VECTOR_CONFIG)},
-        "input": {"dimensions": get_all_dimensions(config=INPUT_VECTOR_CONFIG)},
+        "state": {"dimensions": get_all_dimensions(config=STATE_CONFIG)},
+        "input": {"dimensions": get_all_dimensions(config=INPUT_CONFIG)},
     },
 }
