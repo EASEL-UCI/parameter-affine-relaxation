c8ce3906327e9c7a309d4c770f42a06c4008b11e
Derek Fan <derekfan.18.2002@gmail.com>
2024-10-08 10:32:24 -0400

    combined nl model and aff model mpcs into one example

--- examples/mpc_example.py
+++ examples/mpc_example.py
@@ -2,18 +2,20 @@
 
 import time
 import numpy as np
-from par.models import CrazyflieModel
+from par.models import CrazyflieModel, ParameterAffineQuadrotorModel
 from par.quat import random_unit_quat
 from par.mpc import NMPC
 
 nl_model = CrazyflieModel()
+aff_model = ParameterAffineQuadrotorModel(nl_model.parameters)
 
 Q = np.diag(np.hstack((
     10.0 * np.ones(3), 5.0 * np.ones(4), 1.0 * np.ones(6)
 )))
 R = 0.01 * np.eye(4)
 Qf = 2.0 * Q
-nmpc = NMPC(dt=0.05, N=50, Q=Q, R=R, Qf=Qf, model=nl_model)
+nl_nmpc = NMPC(dt=0.01, N=100, Q=Q, R=R, Qf=Qf, model=nl_model)
+aff_nmpc = NMPC(dt=0.01, N=100, Q=Q, R=R, Qf=Qf, model=aff_model)
 
 pos0 = np.random.uniform(low=-2.0, high=2.0, size=3)
 att0 = random_unit_quat()
@@ -25,9 +27,13 @@ lbu = np.zeros(4)
 ubu = 0.15 * np.ones(4)
 
 st = time.time()
-nmpc.solve(x=x0, lbu=lbu, ubu=ubu)
+nl_nmpc.solve(x=x0, lbu=lbu, ubu=ubu)
 et = time.time()
-
 print(f"computation time: {et-st}")
-nmpc.plot_trajectory()
+nl_nmpc.plot_trajectory()
 
+st = time.time()
+aff_nmpc.solve(x=x0, lbu=lbu, ubu=ubu)
+et = time.time()
+print(f"computation time: {et-st}")
+aff_nmpc.plot_trajectory()
