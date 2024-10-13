8e18a083237bbc7401874a8c21933a0588e02eb6
Derek Fan <derekfan.18.2002@gmail.com>
2024-10-13 14:56:27 -0400

    added example derivation of koopman lifted gravity and attitude dynamics

---
+++ examples/koopman_derivation.py
@@ -0,0 +1,23 @@
+import numpy as np
+import casadi as cs
+
+from par.utils.koopman import attitude, gravity
+from par.constants import GRAVITY
+
+
+v0 = cs.SX.sym("v0", 3)
+J = 2.0 * np.eye(3)
+Nv = 5
+
+vs = attitude.get_vs(v0, J, Nv)
+Hs = attitude.get_Hs(vs, J)
+B = attitude.get_input_matrix(Hs, J)
+
+
+Ng = 3
+vs_Ng = attitude.get_vs(v0, J, Ng)
+Hs_Ng = attitude.get_Hs(vs_Ng, J)
+
+g0 = cs.SX(cs.vertcat(0, 0, -GRAVITY))
+gs = gravity.get_gs(g0, vs_Ng)
+Gs = gravity.get_Gs(gs, vs_Ng, Hs_Ng, J)
