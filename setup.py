a68231c2dce79746920cec5ab91a8871d99c68bf
Derek Fan <derekfan.18.2002@gmail.com>
2024-10-12 21:31:56 -0400

    updated package dependencies

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='par',
-    version='0.0.0',
+    name="par",
+    version="0.0.0",
     packages=find_packages(include=[
-        'par',
-        'par.*'
+        "par",
+        "par.*"
     ]),
     install_requires=[
-        'numpy==1.26.4',
-        'scipy==1.14.1',
-        'matplotlib==3.8.3',
-        'casadi==3.6.3',
+        "casadi==3.6.7",
+        "numpy==2.1.2",
+        "scipy==1.14.1",
+        "matplotlib==3.9.2",
+        "PyQt6==6.7.1"
     ]
 )
