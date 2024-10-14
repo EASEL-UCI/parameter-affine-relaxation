3bd20a06a957cfa6e1346582011f4fba17263471
Derek Fan <derekfan.18.2002@gmail.com>
2024-10-14 16:06:52 -0400

    added velocity observables

---
+++ par/koopman/velocity_observables.py
@@ -0,0 +1,56 @@
+from typing import List
+
+import casadi as cs
+import numpy as np
+
+from par.utils.math import binomial_coefficient
+
+
+def get_velocities(
+    velocity_0: cs.SX,
+    angular_velocities: List[cs.SX],
+) -> List[cs.SX]:
+    velocities = [velocity_0]
+    Nz = len(angular_velocities)
+    for k in range(1, Nz):
+        velocity_k = cs.SX.zeros(3)
+        for n in range(k):
+            velocity_k += binomial_coefficient(k-1, n) * \
+                        cs.skew(angular_velocities[n]).T @ velocities[k-n-1]
+        velocities += [velocity_k]
+    return velocities
+
+
+def get_omegas(
+    velocities: List[cs.SX]
+) -> List[cs.SX]:
+    omegas = [cs.SX.eye(3)]
+    Nz = len(velocities)
+    for k in range(Nz):
+        omega_k = cs.SX.eye(3)
+        for n in range(k):
+            omega_k += binomial_coefficient(k-1, n) * \
+                cs.skew(velocities[n-1]).T @ omegas[k-n-1]
+        omegas += [omega_k]
+    return omegas
+
+
+def get_Vs(
+    velocities: List[cs.SX],
+    angular_velocities: List[cs.SX],
+    Hs: List[cs.SX],
+    J: np.ndarray,
+) -> List[cs.SX]:
+    assert len(velocities) == len(angular_velocities) == len(Hs)
+    Nz = len(velocities)
+    inv_J = np.linalg.inv(J)
+    Vs = [cs.SX.zeros(3,3)]
+    for k in range(1, Nz):
+        Vk = cs.SX.zeros(3,3)
+        for n in range(k):
+            Vk += binomial_coefficient(k-1, n) * \
+                cs.skew(velocities[n]).T @ inv_J @ Hs[k-n-1]
+            Vk += binomial_coefficient(k-1, n) * \
+                cs.skew(angular_velocities[n]).T @ inv_J @ Vs[k-n-1]
+        Vs += [Vk]
+    return Vs
