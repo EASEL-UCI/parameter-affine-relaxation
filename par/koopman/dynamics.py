a2afd34e5462285aa86eddef1ae5b6f97cf6d8ca
Derek Fan <derekfan.18.2002@gmail.com>
2024-10-14 22:30:54 -0400

    added dynamics matrices for attitude and gravity

--- par/koopman/dynamics.py
+++ par/koopman/dynamics.py
@@ -4,14 +4,18 @@ import casadi as cs
 import numpy as np
 
 from par.utils.math import jordan_block
+from par.koopman.observables import attitude, gravity, velocity
 
 
-def get_state_matrix(
+def get_nominal_state_matrix(
     N: int,
 ) -> np.ndarray:
-    return jordan_block(lam=0.0, n=int(3*N))
+    A1 = np.zeros((3*N, 3))
+    A2 = np.eye(3 * (N-1))
+    A3 = np.zeros((3, 3))
+    return np.hstack(( A1, np.vstack((A2, A3)) ))
 
-
+'''
 def get_input_block(
     sub_blocks: List[cs.SX],
     i: int
@@ -21,22 +25,33 @@ def get_input_block(
     for j in range(1, Nv):
         B = cs.vertcat(B, sub_blocks[j][i, :])
     return B
+'''
 
-
-def get_attitude_state_matrix(N: int) -> np.ndarray:
-    return get_state_matrix(N)
+def get_attitude_state_matrix(N: int) -> cs.SX:
+    return get_nominal_state_matrix(N)
 
 
 def get_attitude_input_matrix(
     Hs: List[cs.SX],
     J: np.ndarray,
 ) -> cs.SX:
-    N = len(Hs)
     J_inv = np.linalg.inv(J)
-    for i in range(N):
-        Hs[i] = J_inv @ Hs[i]
+    N = len(Hs)
     B = cs.SX()
-    for i in range(3):
-        B = cs.vertcat(B, get_input_block(Hs, i))
+    for k in range(N):
+        B = cs.vertcat(B, J_inv @ Hs[k])
     return B
 
+
+def get_gravity_state_matrix(N: int) -> cs.SX:
+    return get_nominal_state_matrix(N)
+
+
+def get_gravity_input_matrix(
+    Gs: List[cs.SX],
+) -> cs.SX:
+    N = len(Gs)
+    B = cs.SX()
+    for k in range(N):
+        B = cs.vertcat(B, -Gs[k])
+    return B
