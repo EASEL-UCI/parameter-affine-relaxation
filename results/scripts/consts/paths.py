aefb0b15fbe5fb52cd64edcac6caa8c6f8a2dd4f
Derek Fan <derekfan.18.2002@gmail.com>
2024-11-17 03:11:39 -0500

    added paths

---
+++ results/scripts/consts/paths.py
@@ -0,0 +1,9 @@
+'''
+Replace `FIG_PATH` and `DATA_PATH` with the appropriate global paths on your machine.
+'''
+
+FIG_PATH = '/parameter-affine-relaxation/results/figures'
+DATA_PATH = '/parameter-affine-relaxation/results/data'
+CRAZYFLIE_PATH = '/crazyflie/'
+FUSION_ONE_PATH = '/fusion_one/'
+DJI_FLYCART_PATH = '/dji_flycart/'
